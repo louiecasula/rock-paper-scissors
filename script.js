73db76bd9bd99e97a9c47742021b4598524a2cbf
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-03-01 16:31:45 -0500

    playRound implemented.

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-const options = ["Rock", "Paper", "Scissors"]
+const options = ["rock", "paper", "scissors"]
 
 let getComputerChoice = () => options[Math.floor(Math.random() * 3)]
 
-// console.log(getComputerChoice())
+let playRound = (playerSelection, computerSelection) => {
+    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+        return `You both chose ${playerSelection}! It's a tie!`
+    }
+    switch(playerSelection.toLowerCase() + computerSelection.toLowerCase()) {
+        case("rock" + "scissors"):
+        case("scissors" + "paper"):
+        case("paper" + "rock"):
+            return `You won! ${playerSelection} beats ${computerSelection}!`
+        case("rock" + "paper"):
+        case("scissors" + "rock"):
+        case("paper" + "scissors"):
+            return `You lost. ${computerSelection} beats ${playerSelection}...`
+    }
+  }
+  
+  const playerSelection = "ROCK";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
