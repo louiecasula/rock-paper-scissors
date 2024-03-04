7b4bdb1c0d040686e5139a8d81247695f3f43437
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-03-04 17:23:10 -0500

    Added winner/loser message.

--- script.js
+++ script.js
@@ -2,44 +2,48 @@ const options = ["rock", "paper", "scissors"];
 
 let playerScore = 0, computerScore = 0;
 
+const scoreboard = document.getElementById('scoreboard');
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => playRound(button.id, getComputerChoice()));
+    button.addEventListener('click', () => updateScoreboard());
+});
+
 let getComputerChoice = () => options[Math.floor(Math.random() * 3)];
 
+let updateScoreboard = () => {
+    if (playerScore >= 5) {
+        scoreboard.innerHTML = `<p>YOU WON!<br>P1: ${playerScore} - ${computerScore} :CPU</p>`;
+    }
+    else if (computerScore >= 5) {
+        scoreboard.innerHTML = `<p>YOU LOST...<br>P1: ${playerScore} - ${computerScore} :CPU</p>`;
+    }
+    else {
+        scoreboard.innerHTML = `<p>P1: ${playerScore} - ${computerScore} :CPU</p>`;
+    }
+}
+
 let playRound = (playerSelection, computerSelection) => {
-    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+    if (playerSelection == computerSelection) {
         console.log(`You both chose ${playerSelection}! It's a tie!`);
-        updateScoreboard();
         return;
     }
-    switch(playerSelection.toLowerCase() + computerSelection.toLowerCase()) {
+    switch(playerSelection + computerSelection) {
         case("rock" + "scissors"):
         case("scissors" + "paper"):
         case("paper" + "rock"):
             console.log(`You won! ${playerSelection} beats ${computerSelection}!`);
             playerScore++;
-            updateScoreboard();
-            return true;
+            return;
         case("rock" + "paper"):
         case("scissors" + "rock"):
         case("paper" + "scissors"):
             console.log(`You lost. ${computerSelection} beats ${playerSelection}...`);
             computerScore++;
-            updateScoreboard();
-            return false;
+            return;
         default:
             console.log(`Invalid input. Please enter "Rock", "Paper", or "Scissors".`);
     }
-}
-
-const buttons = document.querySelectorAll('button');
-
-buttons.forEach((button) => {
-
-    button.addEventListener('click', () => playRound(button.id, getComputerChoice()))
-
-});
-
-let updateScoreboard = () => {
-    const scoreboard = document.getElementById('scoreboard');
-
-    scoreboard.innerHTML = `<p>${playerScore} - ${computerScore}</p>`;
 }
